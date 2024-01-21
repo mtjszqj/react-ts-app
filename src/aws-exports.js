677ee64272f91969603a52ec7273111db870cb6d
nobody <nobody@example.com>
2024-01-21 22:29:47 +0800

    feat: add amplify

---
+++ src/aws-exports.js
@@ -0,0 +1,7 @@
+// replace the user pool region, id, and app client id details
+const obj = {
+  "USER_POOL_ID": "ap-northeast-1_JNSXSuYb5",
+  "USER_POOL_APP_CLIENT_ID": "62vgg7hagnpv7g1hq9k11kuj40"
+}
+
+export default obj;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { Amplify } from 'aws-amplify';
+import {
+  fetchAuthSession,
+  getCurrentUser
+} from 'aws-amplify/auth';
+import awsExports from './aws-exports';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: awsExports.USER_POOL_ID,
+      userPoolClientId: awsExports.USER_POOL_APP_CLIENT_ID
+    }
+  }
+})
+
+
+const getAuthenticatedUser = async () => {
+  const {
+    username,
+    signInDetails
+  } = await getCurrentUser();
+
+  const {
+    tokens: session
+  } = await fetchAuthSession();
+
+  // Note that session will no longer contain refreshToken and clockDrift
+  return session?.idToken?.toString();
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Authenticator>
+      {({ signOut, user }) => {
+        getAuthenticatedUser().then(console.log)
+        return (
+          <div>
+            <p>Welcome {user?.username}</p>
+            <button onClick={signOut}>Sign out</button>
+          </div>
+        )
+      }
+      }
+    </Authenticator>
   </React.StrictMode>
 );
 
